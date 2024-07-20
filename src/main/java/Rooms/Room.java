71f96ae21bc446c9affa0ed8116b16b3f175eb00
Mahdi <mahdihosseinzadeh.uut1401@gmail.com>
2024-07-20 10:29:27 +0330

    Adding Rooms Package

---
+++ src/main/java/Rooms/Room.java
@@ -0,0 +1,24 @@
+package Rooms;
+
+public class Room {
+    private final int roomNumber;
+    private final String type;
+    private final double price;
+    private boolean isAvailable;
+
+    public Room(int roomNumber, String type, double price) {
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.price = price;
+        this.isAvailable = true;
+    }
+
+    public int getRoomNumber() {return roomNumber;}
+    public String getType() {return type;}
+    public double getPrice() {return price;}
+    public boolean isAvailable() {return isAvailable;}
+
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+}
