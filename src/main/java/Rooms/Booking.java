bc00d4b75b7cee47396c691594b7b68e9075f893
Mahdi <mahdihosseinzadeh.uut1401@gmail.com>
2024-07-20 10:37:24 +0330

    Updating Rooms Package

---
+++ src/main/java/Rooms/Booking.java
@@ -0,0 +1,34 @@
+package Rooms;
+
+import java.time.LocalDate;
+
+public class Booking {
+    private final Room room;
+    private final Guest guest;
+    private final LocalDate checkInDate;
+    private final LocalDate checkOutDate;
+
+    public Booking(Room room , Guest guest , LocalDate checkInDate , LocalDate checkOutDate){
+        this.room = room;
+        this.guest = guest;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        room.setAvailable(false);
+    }
+
+    public Room getRoom() {return room;}
+    public Guest getGuest() {return guest;}
+    public LocalDate getCheckInDate() {return checkInDate;}
+    public LocalDate getCheckOutDate() {return checkOutDate;}
+
+    public double calculateBill(){
+        long duration = java.time.temporal.ChronoUnit.DAYS.between(checkInDate, checkOutDate);
+        return duration * room.getPrice();
+    }
+
+    public void cancelBooking(){
+        room.setAvailable(true);
+    }
+
+
+}
